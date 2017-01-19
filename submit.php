2e6c9fc0bcc0c0e9ad7cb9ff7604702efd8b7f9b
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2017-01-18 23:51:34 -0500

    Refactored

--- submit.php
+++ submit.php
@@ -1,9 +1,9 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://api.topdecked.me:8080/forms/");
+curl_setopt($ch, CURLOPT_URL, "https://api.topdecked.me/v1/forms/");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_PORT, 8080);
-curl_setopt($ch, CURLOPT_TIMEOUT, 200);
+// curl_setopt($ch, CURLOPT_PORT, 8080);
+curl_setopt($ch, CURLOPT_TIMEOUT, 400);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
